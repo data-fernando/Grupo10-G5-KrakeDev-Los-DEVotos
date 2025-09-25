55c43b0236ed475531c230092f204064b46b9422
Fernando G. Ponce <data.fernan@gmail.com>
2025-09-25 18:15:52 -0500

    lbl resultado agregado, estilos agregados

--- Test/test.js
+++ Test/test.js
@@ -2,6 +2,12 @@ saludar = function(){
     let nombre = recuperarTexto("txtNombre");
     let apellido = recuperarTexto("txtApellido");
     let resultado = generarSaludo(nombre,apellido);
-    console.log(resultado);
+    // console.log(resultado);
+    return resultado
 }
 
+probarFuncion=function(){
+    let mensaje=saludar()
+    mostrarTexto("lblResultado",mensaje)
+
+}
